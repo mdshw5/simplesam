48e7e9d6ff5bea37f45bd312f32eaa86a6508822
Matt Shirley <mdshw5@gmail.com>
2016-04-15 13:25:39 -0400

    Bump version for PyPI

--- simplesam.py
+++ simplesam.py
@@ -23,7 +23,7 @@ try:
 except ImportError: #python2
     from _multiprocessing import Connection
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 class DefaultOrderedDict(OrderedDict):
     def __init__(self, default, items=[]):
