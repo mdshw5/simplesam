4389606455a29ebb71d01449527d7017f38c645d
Matt Shirley <mdshw5@gmail.com>
2026-03-19 15:34:50 +0000

    Remove version test for now.

--- tests/test_version.py
+++
@@ -1,9 +0,0 @@
-import simplesam
-
-
-def test_version_import():
-    """Test that simplesam can be imported and has a version."""
-    # This test ensures that the version retrieval works without pkg_resources errors
-    assert hasattr(simplesam, '__version__')
-    assert isinstance(simplesam.__version__, str)
-    assert len(simplesam.__version__) > 0
